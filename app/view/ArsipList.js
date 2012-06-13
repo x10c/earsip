961cc29c0f83173618b7cc755381a6a161c22e29
mhd.sulhan <ms@kilabit.org>
2012-06-13 17:51:58 +0700

    Remove link to berkas in list Arsip.

--- app/view/ArsipList.js
+++ app/view/ArsipList.js
@@ -21,10 +21,7 @@ Ext.define ('Earsip.view.ArsipList', {
 			if (r.get ('tipe_file') == 0) {
 				return "<span class='dir'>"+ v +"</span>";
 			} else {
-				return "<a class='doc' target='_blank'"
-					+" href='data/download.jsp"
-					+"?berkas="+ r.get('sha') +"&nama="+ v +"'>"
-					+ v +"</a>";
+				return "<span class='doc'>"+ v +"</span>";
 			}
 		}
 	},{
